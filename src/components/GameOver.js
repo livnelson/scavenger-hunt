44d1d012d8de21711b1569284c9b397222fab600
livnelson <livnelson@me.com>
2024-05-16 10:53:15 -0700

    added award

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,34 +1,37 @@
-import React, { useState } from 'react'
-import { useWindowSize } from 'react-use'
-import Confetti from 'react-confetti'
-import '../styles/GameOver.css'
-import '../styles/Stats.css'
+import React, { useState } from 'react';
+import { useWindowSize } from 'react-use';
+import Confetti from 'react-confetti';
+import '../styles/GameOver.css';
+import '../styles/Stats.css';
 
 function GameOver({ gameCode, gameData }) {
-  const API_URL = 'https://jollyrogertelephone.com/8MEBAA7K6yxrnYes5DTwgA7m-md23.php'
-  const [errors, setErrors] = useState([])
-  const { width, height } = useWindowSize()
-  const [username, setUsername] = useState('')
-  const [email, setEmail] = useState('')
+  const API_URL = 'https://jollyrogertelephone.com/8MEBAA7K6yxrnYes5DTwgA7m-md23.php';
+  const [errors, setErrors] = useState([]);
+  const [successMessage, setSuccessMessage] = useState(''); // New state for success message
+  const { width, height } = useWindowSize();
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   
   const handleUserInfo = (e) => {
-    e.preventDefault()
-    // console.log('Name:' + username)
-    // console.log('Email:' + email)
+    e.preventDefault();
     fetch(`${API_URL}/${gameCode}`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ action: 'send_name', gamecode: gameCode, name: username, email: email}),
+      body: JSON.stringify({ action: 'send_name', gamecode: gameCode, name: username, email: email }),
     }).then((r) => {
       if (r.ok) {
         r.json().then((data) => {
-          // console.log(data)
+          setSuccessMessage('Your information was successfully submitted!'); // Set success message
+          setErrors([]); // Clear any previous errors
         })
       } else {
-        r.json().then((err) => setErrors(err.errors))
+        r.json().then((err) => {
+          setErrors(err.errors);
+          setSuccessMessage(''); // Clear success message in case of error
+        })
       }
     })
-  }
+  };
 
   return (
     <div className='game-over'>
@@ -41,7 +44,7 @@ function GameOver({ gameCode, gameData }) {
       />
       <div className='game-over-modal'>
         <h4 className='game-over-header'>Congratulations!</h4>
-        <p className='prize'>YOU ARE A SUPER MONROVIAN!</p>
+        <p className='prize'>YOU KNOW MONROVIA WELL</p>
         <p className='prizes'>
           Please enter your name and email address to be entered into the
           drawing to win a ride for 2 on the Fire Engine during the annual
@@ -70,10 +73,12 @@ function GameOver({ gameCode, gameData }) {
             Submit
           </button>
         </form>
+        {successMessage && <p className='success-message'>{successMessage}</p>}
+        {errors.length > 0 && <p className='error-message'>{errors.join(', ')}</p>}
+        <p className='winner-message'><em>Winner will receive an email with further details within 2 weeks</em></p>
       </div>
-      {errors}
     </div>
-  )
+  );
 }
 
-export default GameOver
+export default GameOver;
